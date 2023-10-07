de5ed0530942b876c3300f68da88c5c4484dcaaa
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2023-10-07 10:45:21 +0200

    notifications

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,29 +4,99 @@ import profile from "@/images/Group 77.png";
 import notification from "@/icons/notification.svg";
 import menu from "@/icons/menu.png";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import pic from "@/images/teacher.png";
+
 const NavBar = ({ openSideBar }: { openSideBar: () => void }) => {
-  const route = useRouter()
+  const route = useRouter();
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav  style={{
-        position: "sticky",
-        top: 0, // Stick to the top of the viewport
-        zIndex: 10, // Optional: You can adjust the z-index as needed
-      }} className="flex px-24 justify-between items-center pt-4 bg-transparent backdrop-blur-md pb-2">
-      <div onClick={() => openSideBar()} className="hover:cursor-pointer">
-        <Image width={40} height={40} alt="menu" src={menu} />
-      </div>
-      <div onClick={()=>route.push("/")} className="hover:cursor-pointer">
-        <Image width={80} height={80} alt="KEYBOX" src={logo} />
-      </div>
-      <div className="flex gap-16 items-center">
-        <span className="hover:cursor-pointer">
-          <Image width={30} height={30} alt="notification" src={notification} />
-        </span>
-        <span className="hover:cursor-pointer">
-          <Image width={20} height={20} alt="profile" src={profile} />
-        </span>
-      </div>
-    </nav>
+    <>
+      {" "}
+      <nav
+        style={{
+          position: "sticky",
+          top: 0, // Stick to the top of the viewport
+          zIndex: 10, // Optional: You can adjust the z-index as needed
+        }}
+        className="flex px-24 justify-between items-center pt-4 bg-transparent backdrop-blur-md pb-2"
+      >
+        <div onClick={() => openSideBar()} className="hover:cursor-pointer">
+          <Image width={40} height={40} alt="menu" src={menu} />
+        </div>
+        <div onClick={() => route.push("/")} className="hover:cursor-pointer">
+          <Image width={80} height={80} alt="KEYBOX" src={logo} />
+        </div>
+        <div className="flex gap-16 items-center">
+          <span onClick={() => setOpen(!open)} className="hover:cursor-pointer">
+            <Image
+              width={30}
+              height={30}
+              alt="notification"
+              src={notification}
+            />
+          </span>
+          <span className="hover:cursor-pointer">
+            <Image width={20} height={20} alt="profile" src={profile} />
+          </span>
+        </div>
+      </nav>
+      {open && (
+        <div className="z-20 w-[40rem] absolute right-8 bg-white drop-shadow-md shadow-secondaryColor p-8 rounded-md">
+          <h2 className="text-secondaryColor text-bold text-3xl pl-8">
+            Notifications
+          </h2>
+          <div className="py-4 px-8">
+            <div className="flex gap-8 pt-8">
+              <Image
+                className="self-start  w-16 h-16"
+                height={80}
+                width={80}
+                src={pic}
+                alt=" teacher"
+              />
+              <div className="flex flex-col">
+                <h2 className="text-secondaryColor font-semibold text-lg">
+                  Task to do
+                </h2>
+                <p className="text-textColor pl-4  pt-4">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Labore quaerat ipsum eveniet. Alias expedita...
+                </p>
+                <span className="flex gap-2 pl-4 pt-4">
+                  <p className="text-mainRed">Deadline :</p>
+                  <p className="text-textColor">22/02/2023</p>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-[1px] bg-thirdColor px-8" />
+          <div className="flex gap-8 pt-8">
+            <Image
+              className="self-start  w-16 h-16"
+              height={80}
+              width={80}
+              src={pic}
+              alt=" teacher"
+            />
+            <div className="flex flex-col">
+              <h2 className="text-secondaryColor font-semibold text-lg">
+                Task to do
+              </h2>
+              <p className="text-textColor pl-4  pt-4">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore
+                quaerat ipsum eveniet. Alias expedita...
+              </p>
+              <span className="flex gap-2 pl-4 pt-4">
+                <p className="text-mainRed">Deadline :</p>
+                <p className="text-textColor">22/02/2023</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/kids/index.tsx
+++ src/pages/kids/index.tsx
@@ -1,40 +1,39 @@
-
-import React, { useState } from 'react';
-import Image from 'next/image';
-import text from '/public/images/Kids.svg';
-import scratch2 from '/public/images/scratch2.svg';
-import Layout from './Layout';
-import item1 from '/public/images/item1.svg';
-import item2 from '/public/images/item2.svg';
-import item3 from '/public/images/item3.svg';
-import vd from '/public/images/vd.svg'
-import play from '/public/images/play.svg'
+import React, { useState } from "react";
+import Image from "next/image";
+import text from "/public/images/Kids.svg";
+import scratch2 from "/public/images/scratch2.svg";
+import Layout from "./Layout";
+import item1 from "/public/images/item1.svg";
+import item2 from "/public/images/item2.svg";
+import item3 from "/public/images/item3.svg";
+import vd from "/public/images/vd.svg";
+import play from "/public/images/play.svg";
 
 const Kids = () => {
   const videoData = [
     {
       id: 1,
-      videoUrl: '../../../public/video.mp4',
-      title: 'Video 1',
-      description: 'Description for Video 1',
+      videoUrl: "../../../public/video.mp4",
+      title: "Video 1",
+      description: "Description for Video 1",
     },
     {
       id: 2,
-      videoUrl: '../../../public/video.mp4',
-      title: 'Video 2',
-      description: 'Description for Video 2',
+      videoUrl: "../../../public/video.mp4",
+      title: "Video 2",
+      description: "Description for Video 2",
     },
     {
       id: 3,
-      videoUrl: '../../../public/video.mp4',
-      title: 'Video 3',
-      description: 'Description for Video 3',
+      videoUrl: "../../../public/video.mp4",
+      title: "Video 3",
+      description: "Description for Video 3",
     },
     {
       id: 4,
-      videoUrl: '../../../public/video.mp4',
-      title: 'Video 4',
-      description: 'Description for Video 4',
+      videoUrl: "../../../public/video.mp4",
+      title: "Video 4",
+      description: "Description for Video 4",
     },
   ];
 
@@ -44,57 +43,56 @@ const Kids = () => {
   );
 
   // Handle mouse enter event for a specific card
-  const handleMouseEnter = (index:any) => {
+  const handleMouseEnter = (index: any) => {
     const updatedIsHoveredArray = [...isHoveredArray];
     updatedIsHoveredArray[index] = true;
     setIsHoveredArray(updatedIsHoveredArray);
   };
 
   // Handle mouse leave event for a specific card
-  const handleMouseLeave = (index:any) => {
+  const handleMouseLeave = (index: any) => {
     const updatedIsHoveredArray = [...isHoveredArray];
     updatedIsHoveredArray[index] = false;
     setIsHoveredArray(updatedIsHoveredArray);
   };
 
-  
-
   return (
     <>
-        <Layout>
-  
-     <Image src={text} alt='text' className='ml-4 relative' >
-  
-     </Image>
-     <div className='relative w-full h-[500px] my-4 ' >
-   
-     <div className='w-[80%]  max-w-[700px] mx-auto  md:h-[300px] h-[200px] drop-shadow shadow-xl rounded-2xl'>
-  <div className='w-1/2 absolute inset-0 h-full overflow-hidden'>
-    <div className='scratch1 rounded-xl'></div>
-  </div>
-  <div className='w-1/2 absolute right-0 h-full flex flex-col justify-start py-4 gap-12 items-center px-2'>
-   <div>
-   <Image src={scratch2} alt='e' className='w-[80%]' />
-    <p className='my-2' >Enjoy this game in order to improve your skills Enjoy this game in order to improve your skills Enjoy this game in order to improve your skills</p>
-   </div>
-    <button type="submit" className="text-white   bg-pink-300 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-3xl text-sm px-8 py-2 ">JOIN</button>
-  </div>
-</div>
+      <Layout>
+        <Image src={text} alt="text" className="ml-4 relative"></Image>
+        <div className="relative w-full h-[500px] my-4 ">
+          <div className="w-[80%]  max-w-[700px] mx-auto  md:h-[300px] h-[200px] drop-shadow shadow-xl rounded-2xl">
+            <div className="w-1/2 absolute inset-0 h-full overflow-hidden">
+              <div className="scratch1 rounded-xl"></div>
+            </div>
+            <div className="w-1/2 absolute right-0 h-full flex flex-col justify-start py-4 gap-12 items-center px-2">
+              <div>
+                <Image src={scratch2} alt="e" className="w-[80%]" />
+                <p className="my-2">
+                  Enjoy this game in order to improve your skills Enjoy this
+                  game in order to improve your skills Enjoy this game in order
+                  to improve your skills
+                </p>
+              </div>
+              <button
+                type="submit"
+                className="text-white   bg-pink-300 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-3xl text-sm px-8 py-2 "
+              >
+                JOIN
+              </button>
+            </div>
+          </div>
+        </div>
 
+        <div className="my-4">
+          <p className="ml-4 text-[#073946] font-medium text-2xl ">
+            Kids Should Have Fun
+          </p>
 
-
-
-
-     </div>
-
-        <div className='my-4' >
-          <p className='ml-4 text-[#073946] font-medium text-2xl ' >Kids Should Have Fun</p>
-
-          <div className='flex justify-between items-center flex-wrap px-4'>
+          <div className="flex justify-between items-center flex-wrap px-4">
             {videoData.map((video, i) => (
               <div
-                className='w-[300px] h-[250px] bg-gray-200 shadow-xl rounded-xl'
-             
+                className="w-[300px] h-[250px] bg-gray-200 shadow-xl rounded-xl"
                 key={video.id}
               >
                 {/* <Image
@@ -105,11 +103,10 @@ const Kids = () => {
                 >
 
                 </Image> */}
-                <div className='bg-yellow-500 w-full h-[150px] flex justify-center items-center' >
-                <Image src={play}  alt='aa' />
+                <div className="bg-yellow-500 w-full h-[150px] flex justify-center items-center">
+                  <Image src={play} alt="aa" />
                 </div>
 
-              
                 {/* <video
                
                   autoPlay={isHoveredArray[i]}
@@ -122,11 +119,10 @@ const Kids = () => {
                 >
                  <source  src='../../../public/vlc-record-2023-09-30-21h26m25s-invideo-ai-1080 Revolutionizing Healthcare_ A Comprehens 2023-09-30.mp4-.mp4' type='video/mp4' />
                 </video> */}
-                <div className='flex flex-col items-start justify-center gap-4 p-4' >
-                <h2 className='text-xl font-semibold' >{video.title}</h2>
-                <p>{video.description}</p>
+                <div className="flex flex-col items-start justify-center gap-4 p-4">
+                  <h2 className="text-xl font-semibold">{video.title}</h2>
+                  <p>{video.description}</p>
                 </div>
-            
               </div>
             ))}
           </div>
